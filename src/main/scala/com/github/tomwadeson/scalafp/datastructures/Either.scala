491d4914f7f75f6d44c1e512f6dcc6fe34c0da07
Tom Wadeson <tom@tomwadeson.com>
2016-08-29 18:44:17 +0100

    Rewrite `isLeft` and `isRight` in terms of `fold`

--- src/main/scala/com/github/tomwadeson/scalafp/datastructures/Either.scala
+++ src/main/scala/com/github/tomwadeson/scalafp/datastructures/Either.scala
@@ -2,21 +2,21 @@ package com.github.tomwadeson.scalafp.datastructures
 
 import Either.{Left, Right}
 
-sealed trait Either[+E, +A] {
-  def isLeft: Boolean = this match {
-    case Left(_) => true
-    case Right(_) => false
-  }
+sealed trait Either[+A, +B] {
+  def isLeft: Boolean =
+    fold(_ => true)(_ => false)
 
-  def isRight: Boolean = !isLeft
+  def isRight: Boolean =
+    fold(_ => false)(_ => true)
 
-  def fold[T](l: E => T)(r: A => T): T = this match {
+  def fold[T](l: A => T)(r: B => T): T = this match {
     case Left(x) => l(x)
     case Right(x) => r(x)
   }
 }
 
 object Either {
-  final case class Left[+E](value: E) extends Either[E, Nothing]
-  final case class Right[+A](value: A) extends Either[Nothing, A]
+  final case class Left[+A](value: A) extends Either[A, Nothing]
+
+  final case class Right[+B](value: B) extends Either[Nothing, B]
 }
