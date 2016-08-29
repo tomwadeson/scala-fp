8e1c4a04764d75a88e6d7228168b58f03a0d09b6
Tom Wadeson <tom@tomwadeson.com>
2016-08-29 11:04:46 +0100

    Enhance List API

--- src/main/scala/com/github/tomwadeson/scalafp/datastructures/List.scala
+++ src/main/scala/com/github/tomwadeson/scalafp/datastructures/List.scala
@@ -2,13 +2,50 @@ package com.github.tomwadeson.scalafp.datastructures
 
 import com.github.tomwadeson.scalafp.typeclasses.{Functor, Monoid}
 
-sealed trait List[+A]
+sealed trait List[+A] {
+  def foldLeft[B](acc: B)(f: (B, A) => B): B
+  def foldRight[B](acc: B)(f: (A, B) => B): B
+  def reverse: List[A]
+  def ++[B >: A](other: List[B]): List[B]
+}
 
 object List {
 
-  final case class Cons[+A](head: A, tail: List[A]) extends List[A]
+  final case class Cons[+A](head: A, tail: List[A]) extends List[A] {
 
-  case object Nil extends List[Nothing]
+    def foldLeft[B](acc: B)(f: (B, A) => B): B = this match {
+      case (Cons(h, t)) => t.foldLeft(f(acc, h))(f)
+    }
+
+    def foldRight[B](acc: B)(f: (A, B) => B): B =
+      this.reverse.foldLeft(acc)((x, y) => f(y, x))
+
+    def reverse: List[A] =
+      this.foldLeft(empty[A])((x, y) => Cons(y, x))
+
+    def ++[B >: A](other: List[B]): List[B] =
+      this.foldRight(other)(Cons(_, _))
+  }
+
+  case object Nil extends List[Nothing] {
+    def foldLeft[B](acc: B)(f: (B, Nothing) => B): B =
+      acc
+
+    def foldRight[B](acc: B)(f: (Nothing, B) => B): B =
+      acc
+
+    def reverse: List[Nothing] =
+      this
+
+    def ++[B >: Nothing](other: List[B]): List[B] =
+      other
+  }
+
+  def apply[A](elems: A*): List[A] =
+    elems.foldRight(empty[A])(Cons(_, _))
+
+  def empty[A]: List[A] =
+    Nil
 
   // Typeclass instances
 
--- src/test/scala/com/github/tomwadeson/scalafp/datastructures/ListSpec.scala
+++ src/test/scala/com/github/tomwadeson/scalafp/datastructures/ListSpec.scala
@@ -5,22 +5,42 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class ListSpec extends FlatSpec with Matchers {
 
-  "List" should "provide a Monoid instance" in {
+  "List" should "define foldLeft" in {
+    val xs = List(1,2,3)
+    val sumOfList = xs.foldLeft(0)(_ + _)
+    sumOfList should be(6)
+  }
+
+  it should "define foldRight" in {
+    val xs = List(1,2,3)
+    val sumOfList = xs.foldRight(0)(_ + _)
+    sumOfList should be(6)
+  }
+
+  it should "define reverse" in {
+    List(1,2,3).reverse should be(List(3,2,1))
+  }
+
+  it should "define append" in {
+    List(1,2,3) ++ List(4,5,6) should be(List(1,2,3,4,5,6))
+  }
+
+  it should "provide a Monoid instance" in {
     import com.github.tomwadeson.scalafp.typeclasses.Monoid.ops._
 
-    val xs: List[Int] = Cons(1, Cons(2, Nil))
-    val ys: List[Int] = Cons(3, Cons(4, Nil))
+    val xs = List(1,2)
+    val ys = List(3,4)
 
-    xs |+| ys should be (Cons(1, Cons(2, Cons(3, Cons(4, Nil)))))
+    xs |+| ys should be (List(1,2,3,4))
     xs |+| Nil should be(xs)
-    (Nil: List[Int]) |+| xs should be (xs)
+    List.empty[Int] |+| xs should be (xs)
   }
 
   it should "provide a Functor instance" in {
     import com.github.tomwadeson.scalafp.typeclasses.Functor.ops._
 
-    val xs: List[Int] = Cons(1, Cons(2, Cons(3, Nil)))
-    xs.map(_ * 2) should be(Cons(2, Cons(4, Cons(6, Nil))))
-    (Nil: List[Int]).map(_ * 2) should be(Nil)
+    val xs = List(1,2,3)
+    xs.map(_ * 2) should be(List(2,4,6))
+    List.empty[Int].map(_ * 2) should be(Nil)
   }
 }
