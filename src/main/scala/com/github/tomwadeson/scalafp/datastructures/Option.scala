dec0549d5cae60c51c014ae811959510cdc49985
Tom Wadeson <tom@tomwadeson.com>
2016-08-29 16:30:33 +0100

    Option is an Applicative

--- src/main/scala/com/github/tomwadeson/scalafp/datastructures/Option.scala
+++ src/main/scala/com/github/tomwadeson/scalafp/datastructures/Option.scala
@@ -1,5 +1,7 @@
 package com.github.tomwadeson.scalafp.datastructures
 
+import com.github.tomwadeson.scalafp.typeclasses.MonadPlus
+
 sealed trait Option[+A] {
   def getOrElse[B >: A](default: B): B =
     this.fold(default)(identity)
@@ -20,4 +22,21 @@ object Option {
 
   def apply[A](value: A): Option[A] =
     if (value == null) None else Some(value)
+
+  implicit val MonadPlusInstance = new MonadPlus[Option] {
+    def empty[A]: Option[A] =
+      None
+
+    def filter[A](fa: Option[A])(p: (A) => Boolean): Option[A] =
+      fa.fold(empty[A])(x => if (p(x)) Some(x) else empty[A])
+
+    def flatMap[A, B](fa: Option[A])(f: (A) => Option[B]): Option[B] =
+      map(fa)(f).fold(empty[B])(identity)
+
+    def pure[A](a: A): Option[A] =
+      Some(a)
+
+    override def map[A, B](fa: Option[A])(f: (A) => B): Option[B] =
+      fa.fold(empty[B])(x => Some(f(x)))
+  }
 }
--- src/test/scala/com/github/tomwadeson/scalafp/datastructures/OptionSpec.scala
+++ src/test/scala/com/github/tomwadeson/scalafp/datastructures/OptionSpec.scala
@@ -1,16 +1,32 @@
 package com.github.tomwadeson.scalafp.datastructures
 
 import com.github.tomwadeson.scalafp.datastructures.Option.{None, Some}
+import com.github.tomwadeson.scalafp.typeclasses.Applicative
 import org.scalatest.{FlatSpec, Matchers}
 
 class OptionSpec extends FlatSpec with Matchers {
 
-  "Option" should "define getOrElse" in {
-    val x: Option[Int] = Some(1)
-    val y: Option[Int] = None
+  val x: Option[Int] = Some(1)
+  val y: Option[Int] = None
 
+  "Option" should "define getOrElse" in {
     x.getOrElse(42) should be(1)
     y.getOrElse(42) should be(42)
   }
 
+  it should "provide a Functor instance" in {
+    import com.github.tomwadeson.scalafp.typeclasses.Functor.ops._
+
+    x.map(_ + 1) should be(Some(2))
+    y.map(_ + 1) should be(None)
+  }
+
+  it should "provide an Applicative instance" in {
+    import com.github.tomwadeson.scalafp.typeclasses.Applicative.ops._
+
+    val f: Option[Int => Int] = Applicative[Option].pure(_ + 10)
+
+    x <*> f should be(Some(11))
+    y <*> f should be(None)
+  }
 }
